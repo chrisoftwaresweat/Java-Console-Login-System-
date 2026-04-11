1561948ea7321343dd90f0085bb643b038d2dd85
Charles Henry Tinoy <charleshenrytinoy8@gmail.com>
2026-04-11 12:59:12 +0800

    Added options for signing up and signing in.

--- src/Main.java
+++ src/Main.java
@@ -1,15 +1,51 @@
 import java.util.Scanner;
 
 public class Main {
+    public static void displayBorder(int length) {
+        for (int i = 0; i < length; i++)
+            System.out.print("=");
+        System.out.println();
+    }
+
+    public static void displayBorder(int length, String heading) {
+        for (int i = 0; i < length / 2; i++)
+            System.out.print("=");
+        System.out.printf(" %s ", heading);
+        for (int i = 0; i < length / 2; i++)
+            System.out.print("=");
+        System.out.println();
+    }
+
+    private static void displayOptions() {
+        displayBorder(10, "Options");
+        System.out.println("SIGN UP\t\t\t[1]");
+        System.out.println("SIGN IN\t\t\t[2]");
+        System.out.println("EXIT\t\t\t[3]");
+        displayBorder(10, "Options");
+    }
+
     public static void main(String[] args){
         Scanner scanner = new Scanner(System.in);
         Account account = new Account();
 
-        //sign up
-        account.createAcc(scanner);
+        boolean isExit = false;
+        do {
+            displayOptions();
 
-        //sign in
-        account.loginAcc(scanner);
+            System.out.print("Option: ");
+            while (!scanner.hasNextInt()) {
+                displayOptions();
+                System.out.print("Invalid input, please try again: ");
+                scanner.next();
+            }
+            int option = scanner.nextInt();
+
+            switch (option) {
+                case 1 -> account.createAcc(scanner);
+                case 2 -> account.loginAcc(scanner);
+                default -> isExit = true;
+            }
+        } while (!isExit);
 
         scanner.close();
     }
